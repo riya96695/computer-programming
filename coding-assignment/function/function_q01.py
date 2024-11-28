c365a41dc50bd8daaa11991b692fc9df442b419c
riya96695 <rk8273531521@gmail.com>
2024-11-28 22:24:39 +0530

    function_q01.py

---
+++ coding-assignment/function/function_q01.py
@@ -0,0 +1,6 @@
+"Question: Write a function that takes a number and returns whether it's even or odd."
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
