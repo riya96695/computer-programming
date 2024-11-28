13e83fe4fc86f335fa1ae20766a6420e01772f7c
riya96695 <rk8273531521@gmail.com>
2024-11-28 23:27:58 +0530

    list_8.py

--- coding-assignment/list/list_8.py
+++ coding-assignment/list/list_8.py
@@ -1,1 +1,8 @@
 
+"Write a Python program to find the length of a string without using the len() function."
+
+def string_length(s):
+    count = 0
+    for char in s:
+        count += 1
+    return count
