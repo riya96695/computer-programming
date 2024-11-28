c853c6c5fe0203a4ceb2628e0a2f6e670b5aa948
riya96695 <rk8273531521@gmail.com>
2024-11-28 21:38:30 +0530

    string_13.py

---
+++ coding-assignment/string/string_13.py
@@ -0,0 +1,5 @@
+"Find the position of a substring in a string"
+def find_substring_position(string, substring):
+    return string.find(substring)
+
+print(find_substring_position("Hello Python World", "Python"))
