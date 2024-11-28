489ba50546b57e3fc135645ed2a9298b7709ac9e
riya96695 <rk8273531521@gmail.com>
2024-11-28 22:32:31 +0530

    func_6.py

---
+++ coding-assignment/function/func_6.py
@@ -0,0 +1,6 @@
+"Write a function that returns the Fibonacci sequence up to n terms."
+def fibonacci(n):
+    fib_seq = [0, 1]
+    for i in range(2, n):
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+    return fib_seq[:n]
